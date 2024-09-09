f5f1a3474f4c4385c6a4c0406a30ebfd44500737
Aweinman <aminweinman@gmail.com>
2024-09-08 18:41:57 -0600

    base chatbot

--- Main.py
+++ Main.py
@@ -1,30 +1,20 @@
 import ollama
+import openai
 
-def chat_with_llama3():
-    print("************************")
-    print("Welcome to llama3 chatbot")
-    print("Enter 'exit' to leave the chat")
-    print("************************",end="")
-    while True:
-        user_input = input("\nYou: ")
-        if user_input.lower() == 'exit':
-            print("Ending chat. Goodbye!")
-            break
-        llama_response = send_to_llama3(user_input)
-        print("Llama-3: " + llama_response)
+openai.api_key = "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"
 
-def send_to_llama3(user_input):
-    llama_response = ollama.chat(
-    model="llama3",
-    messages=[
-        {
-            "role": "user",
-            "content": user_input,
-        },
-    ],
+def chat_main(prompt):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role": "user", "content": prompt}]
     )
-  
-    return llama_response["message"]["content"]
-
-# Start the chat
-chat_with_llama3()
+    return response.choices[0].messages.content.strip()
+if __name__ == "__main__":
+    while(True):
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+        
+        response = chat_main(user_input)
+        print("Chat: ", response)
+    
